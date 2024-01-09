4c7a1877861241e0055ed3eb0776efd220453b01
AudiHagi <audi.hagi2002@gmail.com>
2024-01-09 16:10:45 +0700

    change: add theme color, add some validation

--- src/components/AnimeList/header.js
+++ src/components/AnimeList/header.js
@@ -3,9 +3,9 @@ import Link from "next/link"
 const Header = ({ title, linkHref, linkTitle }) => {
     return (
         <div className="p-4 flex justify-between items-center">
-            <h1 className="text-2xl font-bold">{title}</h1>
+            <h1 className="text-2xl font-bold text-color-primary">{title}</h1>
             {linkHref && linkTitle ?
-                <Link href={linkHref} className="md:text-xl text-md underline transition-all">{linkTitle}</Link>
+                <Link href={linkHref} className="md:text-xl text-md underline transition-all text-color-primary">{linkTitle}</Link>
                 : null
             }
         </div>
--- src/components/Navbar/InputSearch.js
+++ src/components/Navbar/InputSearch.js
@@ -8,17 +8,19 @@ const InputSearch = () => {
     const search_ref = useRef()
     const router = useRouter()
     const handle_search = (event) => {
-        event.preventDefault()
-        const keyword = search_ref.current.value
-        router.push(`/search/${keyword}`)
+        if (event.key === "Enter" || event.type == "click") {
+            const keyword = search_ref.current.value
+            if (keyword != "") {
+                event.preventDefault()
+                router.push(`/search/${keyword}`)
+            }else{
+            }
+        }
     }
+
     return (
         <div className="relative">
-            <input placeholder="Cari Anime..." className="w-full p-2 rounded" ref={search_ref} onKeyDown={(event) => {
-                if (event.key === 'Enter') {
-                    handle_search(event);
-                }
-            }}></input>
+            <input placeholder="Cari Anime..." className="w-full p-2 rounded" ref={search_ref} onKeyDown={handle_search}></input>
             <button className="absolute top-2 end-2" onClick={handle_search}>
                 <MagnifyingGlass size={24} />
             </button>
